7625be8c16861ab34f3cde8e3a330c46c6fae80f
Christian Mollekopf <mollekopf@kolabsys.com>
2019-07-11 22:16:55 +0200

    First batch of utility functions

--- src/Utils.php
+++ src/Utils.php
@@ -7,8 +7,61 @@ namespace Phlesk;
  */
 class Utils
 {
-    public static function test()
+    private static function getSubscriptionDomains(pm_Domain $domain)
     {
-        echo "test";
+        // No way up to the subscription, go through client
+        $client = $domain->getClient();
+        $homepath = $domain->getHomePath();
+
+        $subscription_domains = [];
+        $domains = pm_Domain::getDomainsByClient($client);
+        foreach ($domains as $d) {
+            if ($d->getHomePath() == $homepath) {
+                $subscription_domains[] = $d;
+            }
+        }
+
+        return $subscription_domains;
+    }
+
+    private static function isSubscriptionManagementAvailable()
+    {
+        // Determine the Plesk edition -- or rather whether subscription
+        // management is available.
+        $properties = (new \pm_License())->getProperties();
+        if (!$properties['can-manage-customers'] || !$properties['can-manage-resellers']) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Set the permission default value depending plesk edition.
+     */
+    private static function initPermissionDefault()
+    {
+        if (!isSubscriptionManagementAvailable()) {
+            pm_Settings::set('permission-default', 1);
+        } else {
+            // Without any domains, we can set the permission systems' default
+            // to TRUE. See T27844.
+            $perm_config = pm_Settings::get('permissions-default', null);
+
+            // If permissions were not set
+            if ($perm_config === null) {
+                $domains = pm_Domain::getAllDomains(true);
+
+                if (count($domains) == 0) {
+                    pm_Settings::set('permission-default', 1);
+                } else {
+                    pm_Settings::set('permission-default', 0);
+                }
+            } else {
+                pm_Settings::set('permission-default', $perm_config);
+            }
+        }
+
+        // Remove legacy permissions-default
+        pm_Settings::set('permissions-default', null);
     }
 }
