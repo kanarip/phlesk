a34bc51d5c57d5f06fc224223dc321151070e3d3
Christian Mollekopf <mollekopf@kolabsys.com>
2019-07-11 22:27:49 +0200

    Escape our namespace

--- src/Utils.php
+++ src/Utils.php
@@ -14,7 +14,7 @@ class Utils
         $homepath = $domain->getHomePath();
 
         $subscription_domains = [];
-        $domains = pm_Domain::getDomainsByClient($client);
+        $domains = \pm_Domain::getDomainsByClient($client);
         foreach ($domains as $d) {
             if ($d->getHomePath() == $homepath) {
                 $subscription_domains[] = $d;
@@ -41,27 +41,27 @@ class Utils
     public static function initPermissionDefault()
     {
         if (!self::isSubscriptionManagementAvailable()) {
-            pm_Settings::set('permission-default', 1);
+            \pm_Settings::set('permission-default', 1);
         } else {
             // Without any domains, we can set the permission systems' default
             // to TRUE. See T27844.
-            $perm_config = pm_Settings::get('permissions-default', null);
+            $perm_config = \pm_Settings::get('permissions-default', null);
 
             // If permissions were not set
             if ($perm_config === null) {
-                $domains = pm_Domain::getAllDomains(true);
+                $domains = \pm_Domain::getAllDomains(true);
 
                 if (count($domains) == 0) {
-                    pm_Settings::set('permission-default', 1);
+                    \pm_Settings::set('permission-default', 1);
                 } else {
-                    pm_Settings::set('permission-default', 0);
+                    \pm_Settings::set('permission-default', 0);
                 }
             } else {
-                pm_Settings::set('permission-default', $perm_config);
+                \pm_Settings::set('permission-default', $perm_config);
             }
         }
 
         // Remove legacy permissions-default
-        pm_Settings::set('permissions-default', null);
+        \pm_Settings::set('permissions-default', null);
     }
 }
